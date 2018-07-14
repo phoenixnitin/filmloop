f9bf0f16e2a3dfab4e7bc9a2114302b5536bd806
Nitin <nitinkumar6912@gmail.com>
2018-07-14 16:10:09 +0530

    add: questions at the end of membership plans

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -13,6 +13,7 @@ export class HomeComponent implements OnInit, AfterViewInit{
     let floopHeight = jQuery("#filmloop").height();
     jQuery("#filmloop").css("margin-top", (winHeight - floopHeight)/2);
     jQuery(".container-block").height(jQuery(window).height());
+    jQuery(".ques-container").eq(1).height(jQuery(".ques-container").first().height());
   }
   ngAfterViewInit(){
   }
