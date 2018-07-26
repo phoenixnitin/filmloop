8496c0698e0eb2a0a1c91ee1bc0646fcd79aad5c
Prayas-Agrawal <38398351+Prayas-Agrawal@users.noreply.github.com>
2018-07-26 07:00:43 +0000

    Add files via upload

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,10 +8,18 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
 export class HomeComponent implements OnInit, AfterViewInit{
   constructor(){}
   ngOnInit(){
-    let docHeight = jQuery(document).height();
+    let winHeight = jQuery(window).height();
+    let winWidth = jQuery(window).width();
     let floopHeight = jQuery("#filmloop").height();
-    jQuery("#filmloop").css("margin-top", (docHeight - floopHeight)/2);
+    jQuery("#filmloop").css("margin-top", (winHeight - floopHeight)/2);
+    jQuery(".container-block").height(jQuery(window).height());
+    jQuery(".ques-container").eq(1).height(jQuery(".ques-container").first().height());
+    jQuery(".our-pricing-ques-container").eq(1).height(jQuery(".our-pricing-ques-container").first().height());
   }
   ngAfterViewInit(){
   }
 }
+
+
+
+
