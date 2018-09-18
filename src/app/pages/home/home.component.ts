ed02e47e0b7ff6c05ba117622f219dd5e9612b0e
Nived <nivedn3@gmail.com>
2018-09-18 21:14:42 +0530

    changes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,22 +1,9 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
-
-@Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
-})
-export class HomeComponent implements OnInit, AfterViewInit{
-  constructor(){}
-  ngOnInit(){
-    let winHeight = jQuery(window).height();
-    let winWidth = jQuery(window).width();
-    let floopHeight = jQuery("#filmloop").height();
-    jQuery("#filmloop").css("margin-top", (winHeight - floopHeight)/2);
-    jQuery(".container-block").height(jQuery(window).height());
-    jQuery(".ques-container").eq(1).height(jQuery(".ques-container").first().height());
-    jQuery(".our-pricing-ques-container").eq(1).height(jQuery(".our-pricing-ques-container").first().height());
-  }
-  ngAfterViewInit(){
-  }
-}
-
+$(document).ready(function(){
+    var winHeight = jQuery(window).height();
+    var winWidth = jQuery(window).width();
+    var floopHeight = jQuery("#filmloop").height();
+    $("#filmloop").css("margin-top", (winHeight - floopHeight)/2);
+    $(".container-block").height(jQuery(window).height());
+    $(".ques-container").eq(1).height(jQuery(".ques-container").first().height());
+    $(".our-pricing-ques-container").eq(1).height(jQuery(".our-pricing-ques-container").first().height());
+});
