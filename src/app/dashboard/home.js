73ddfdd06c6e3102e5e12ed2610eb71b86605d7e
Nived <nivedn3@gmail.com>
2018-10-04 20:37:02 +0530

    regimage3.html

---
+++ src/app/dashboard/home.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    var winHeight = $(window).height();
+    var winWidth = $(window).width();
+    var floopHeight = $("#filmloop").height();
+    $("#filmloop").css("margin-top", (winHeight - floopHeight)/2);
+    $(".container-block").height($(window).height());
+    $(".ques-container").eq(1).height($(".ques-container").first().height());
+    $(".our-pricing-ques-container").eq(1).height($(".our-pricing-ques-container").first().height());
+});
+
+
+
+
